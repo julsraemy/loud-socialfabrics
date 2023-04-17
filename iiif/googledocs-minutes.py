65adedf2554a6ebb8258478f2db1911bfe6e56b4
Julien A. Raemy <julien.raemy@gmail.com>
2023-04-17 14:02:18 +0200

    add google credentials

--- iiif/googledocs-minutes.py
+++ iiif/googledocs-minutes.py
@@ -1,97 +1,41 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import csv
+import os
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 
-"""
-Recursively extracts the text from a Google Doc.
-"""
-import googleapiclient.discovery as discovery
-from httplib2 import Http
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
+# Set up Google Drive API credentials
+creds = Credentials.from_authorized_user_file('loud-social-fabrics-e0eac5ffd820.json', ['https://www.googleapis.com/auth/drive'])
 
-SCOPES = 'https://www.googleapis.com/auth/documents.readonly'
-DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
-DOCUMENT_ID = '1hxAmsC1E8b2wAmhdna5sgyvWKQrwsThJ5XSGfKxqupw'
+# Create a service for the Google Drive API
+drive_service = build('drive', 'v3', credentials=creds)
 
+# Define the folder ID of the Google Drive folder containing the Google Docs
+folder_id = '0B_Alni5J8UNITzlpYW1MdnFpSlU'
 
-def get_credentials():
-    """Gets valid user credentials from storage.
+# Get the list of files in the Google Drive folder
+query = f"parents = '{folder_id}' and mimeType = 'application/vnd.google-apps.document'"
+results = drive_service.files().list(q=query, fields="nextPageToken, files(id, name)").execute()
+files = results.get('files', [])
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth 2.0 flow is completed to obtain the new credentials.
+# Loop through each Google Doc and extract paragraphs
+all_data = []
+for file in files:
+    doc_id = file['id']
+    doc_name = file['name']
+    doc = drive_service.files().export(fileId=doc_id, mimeType='text/plain').execute()
+    paragraphs = doc.split('\n\n')  # Assumes paragraphs are separated by two line breaks
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    store = file.Storage('token.json')
-    credentials = store.get()
+    # Append paragraphs to the all_data list
+    data = [doc_name] + paragraphs  # Include the doc name as the first column
+    all_data.append(data)
 
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        credentials = tools.run_flow(flow, store)
-    return credentials
+print('Finished extracting data from all Google Docs.')
 
-def read_paragraph_element(element):
-    """Returns the text in the given ParagraphElement.
+# Write data to CSV file
+output_file = 'output.csv'
+with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Document Name', 'Paragraph 1', 'Paragraph 2', 'Paragraph 3'])  # Add more columns for additional paragraphs
+    writer.writerows(all_data)
 
-        Args:
-            element: a ParagraphElement from a Google Doc.
-    """
-    text_run = element.get('textRun')
-    if not text_run:
-        return ''
-    return text_run.get('content')
-
-
-def read_structural_elements(elements):
-    """Recurses through a list of Structural Elements to read a document's text where text may be
-        in nested elements.
-
-        Args:
-            elements: a list of Structural Elements.
-    """
-    text = ''
-    for value in elements:
-        if 'paragraph' in value:
-            elements = value.get('paragraph').get('elements')
-            for elem in elements:
-                text += read_paragraph_element(elem)
-        elif 'table' in value:
-            # The text in table cells are in nested Structural Elements and tables may be
-            # nested.
-            table = value.get('table')
-            for row in table.get('tableRows'):
-                cells = row.get('tableCells')
-                for cell in cells:
-                    text += read_structural_elements(cell.get('content'))
-        elif 'tableOfContents' in value:
-            # The text in the TOC is also in a Structural Element.
-            toc = value.get('tableOfContents')
-            text += read_structural_elements(toc.get('content'))
-    return text
-
-
-def main():
-    """Uses the Docs API to print out the text of a document."""
-    credentials = get_credentials()
-    http = credentials.authorize(Http())
-    docs_service = discovery.build(
-        'docs', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC)
-    doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
-    doc_content = doc.get('body').get('content')
-    print(read_structural_elements(doc_content))
-
-if __name__ == '__main__':
-    main()
+print(f'Successfully wrote data to {output_file}.')
