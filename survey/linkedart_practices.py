b7e56794736fb4d0a5ca13b6ad98d3dd4974a3f7
Julien A. Raemy <julien.raemy@gmail.com>
2023-06-25 14:35:11 +0200

    add comment

--- survey/linkedart_practices.py
+++ survey/linkedart_practices.py
@@ -1,49 +1,44 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Define the data
-support_channels = ["Community Calls", "Slack", "Mailing List", "GitHub Issues", "Direct Communication (Robert Sanderson)", "Workshops"]
-tools_reference = ["Documentation", "GitHub Issues", "Tools and Reference Materials", "Collaboration (Sharing Experiences, Posting Messages, etc.)"]
+# This is an example of a matrix, not the real data...
 
-# Define the frequency matrix
+support_channels = ["Community Calls", "Slack", "Mailing List", "Robert Sanderson"]
+tools_resources = ["Documentation", "GitHub", "Workshops", "Reference Materials"]
+
 frequency_matrix = np.array([
-    [5, 0, 0, 0],
-    [1, 2, 3, 0],
-    [0, 0, 0, 0],
-    [0, 6, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 1]
+    [8, 5, 6, 3],
+    [6, 3, 2, 1],
+    [2, 1, 4, 3],
+    [4, 2, 3, 7]
 ])
 
-# Set up the figure and axes
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots()
+im = ax.imshow(frequency_matrix, cmap='Blues')
 
-# Create the heatmap
-heatmap = ax.imshow(frequency_matrix, cmap="Blues")
+# Set x-axis labels
+ax.set_xticks(np.arange(len(tools_resources)))
+ax.set_xticklabels(tools_resources, rotation=45, ha='right')
 
-# Set the tick labels and positions
-ax.set_xticks(np.arange(len(tools_reference)))
+# Set y-axis labels
 ax.set_yticks(np.arange(len(support_channels)))
-ax.set_xticklabels(tools_reference, rotation=45, ha="right")
 ax.set_yticklabels(support_channels)
 
-# Loop over the data and create text annotations
-for i in range(len(support_channels)):
-    for j in range(len(tools_reference)):
-        text = ax.text(j, i, frequency_matrix[i, j], ha="center", va="center", color="black", fontsize=9)
-
 # Set the colorbar
-cbar = plt.colorbar(heatmap)
-cbar.set_label("Frequency", rotation=90)
+cbar = ax.figure.colorbar(im, ax=ax)
+cbar.ax.set_ylabel("Frequency", rotation=-90, va="bottom")
 
-# Set the title and axis labels
-plt.title("Support Channels for Linked Art Challenges and Tools and Reference Materials")
-plt.xlabel("Tools and Reference Materials")
-plt.ylabel("Support Channels")
+# Add value annotations
+for i in range(len(support_channels)):
+    for j in range(len(tools_resources)):
+        text = ax.text(j, i, frequency_matrix[i, j], ha='center', va='center', color='black')
+
+# Adjust figure layout to prevent labels from being cut off
+plt.tight_layout()
 
 # Save the figure as SVG and PDF
-plt.savefig("heatmap.svg", format="svg")
-plt.savefig("heatmap.pdf", format="pdf")
+plt.savefig("linkedart_practices.svg")
+plt.savefig("linkedart_practices.pdf")
 
-# Show the heatmap
+# Show the figure
 plt.show()
