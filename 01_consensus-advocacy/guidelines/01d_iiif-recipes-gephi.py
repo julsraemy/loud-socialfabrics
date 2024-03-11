e196b72985498428d9f449460d6fd1b1228627fc
Julien A. Raemy <julien.raemy@gmail.com>
2024-03-11 12:47:57 +0100

    add nodes for gephi

--- 01_consensus-advocacy/guidelines/01d_iiif-recipes-gephi.py
+++ 01_consensus-advocacy/guidelines/01d_iiif-recipes-gephi.py
@@ -2,22 +2,23 @@ import pandas as pd
 import networkx as nx
 import os
 
-# Same setup for script directory and CSV path
+# Load the CSV file
 script_dir = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(script_dir, 'recipes', 'iiif_recipes.csv')
-
 df = pd.read_csv(csv_path)
 
 # Create a new graph
 G = nx.Graph()
 
-# Populate the graph
+# Add nodes and edges from the DataFrame
 for _, row in df.iterrows():
     recipe_node = f"{row['id']}_{row['slug']}"
     viewer_node = row['viewer']
-    G.add_node(recipe_node, label=row['recipe'], type='recipe')
+    # Node attributes
+    G.add_node(recipe_node, label=row['recipe'], slug=row['slug'], type='recipe', url=row['url'])
     G.add_node(viewer_node, type='viewer')
-    G.add_edge(recipe_node, viewer_node, support=row['support'])
+    # Edge attributes
+    G.add_edge(recipe_node, viewer_node, support=row['support'], property=row['property'])
 
 # Write the graph to a GEXF file for use in Gephi
 gexf_path = os.path.join(script_dir, 'recipes', 'graph_for_gephi.gexf')
